b08c49a72626a4e601abf3ea9af1f1fe891b6a3c
mustaphaabdulbasit35 <mustaphaabdulbasit335@gmail.com>
2023-09-13 08:56:41 -0700

    builting.c

--- builting.c
+++ builting.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
+ * _myexit - exits the shell.
  * @info: Structure containing potential arguments. Used to maintain
  * constant function prototype.
- * Return: exits with a given exit status
+ * Return: exits with a given exit status.
  * (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
@@ -30,10 +30,10 @@ int _myexit(info_t *info)
 }
 
 /**
- * _mycd - changes the current directory of the process
+ * _mycd - changes the current directory of the process.
  * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- * Return: Always 0
+ * constant function prototype..
+ * Return: 0 (success)
  */
 int _mycd(info_t *info)
 {
